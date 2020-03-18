7699317c1dab9400aecdeeaf25030dd519ae8f3e
mazurz <mazurz@BY1926A75000213.wit.private>
2020-03-18 12:51:54 -0400

    Started

--- src/Player.java
+++ src/Player.java
@@ -2,4 +2,16 @@
 public class Player {
 
 	private String name;
+	private String character;
+	
+	public Player(String name, String character)
+	{
+		this.name = name;
+		this.character = character;
+	}
+	
+	public void guessing()
+	{
+		
+	}
 }
