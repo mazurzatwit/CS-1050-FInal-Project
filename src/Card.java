51d4a5fc1f30708763e01f60f1751ad345ca193c
sarahsingh <sarahsingh@sarahs-mbp.home>
2020-03-24 15:31:53 -0400

    arraylists creation

--- src/Card.java
+++ src/Card.java
@@ -1,8 +1,19 @@
 import java.util.ArrayList;
 
 public abstract class Card {
-	private ArrayList<Characters> characters = new ArrayList<>();
-
+	
+	private void Character(){
+		ArrayList<Characters> characters = new ArrayList<>();
+	}
+	
+	private static void Rooms(){
+		ArrayList<Rooms> rooms = new ArrayList<>();
+	}
+	
+	private void Weapons() {
+		ArrayList<Weapons> weapons = new ArrayList<>();
+	}
+	
 	public Card()
 	{
 		
