7f190ebfb57e4e47ae47d29fb2498c324e76dd8b
mazurz <mazurz@BY1926A75000213.wit.private>
2020-03-24 15:43:47 -0400

    Update

--- src/Card.java
+++ src/Card.java
@@ -5,6 +5,7 @@ public abstract class Card {
 	private void Character(){
 		ArrayList<Characters> characters = new ArrayList<>();
 		characters.add(new Characters("Mrs.White", "White"));
+		characters.add(new Characters("Professor Plum", "Purple"));
 	}
 	
 	private static void Rooms(){
