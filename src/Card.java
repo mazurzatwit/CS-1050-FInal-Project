2fd52610dcb21b3401bb0a215ddbd158672f671b
sarahsingh <sarahsingh@sarahs-mbp.home>
2020-03-24 17:22:23 -0400

    final arrangement before it messed up

--- src/Card.java
+++ src/Card.java
@@ -3,38 +3,41 @@ import java.util.Random;
 
 public abstract class Card {
 	private Random rand = new Random();
+	private ArrayList<Characters> characters = new ArrayList<>();
+	private ArrayList<Rooms> rooms = new ArrayList<>();
+	private ArrayList<Weapons> weapons = new ArrayList<>();
 	
-	private void Character(){
-		ArrayList<Characters> characters = new ArrayList<>();
-		characters.add(new Characters("Mrs.White", "White"));
-		characters.add(new Characters("Professor Plum", "Purple"));
-		characters.add(new Characters("Colonel Mustard", "Yellow"));
-		characters.add(new Characters("Mrs.Peacock", "Blue"));
-		characters.add(new Characters("Miss.Scarlet", "Red"));
-		characters.add(new Characters("Mr.Green", "Green"));
+	private ArrayList<Characters> Characters(ArrayList<Characters> c){
+		c.add(new Characters("Mrs.White", "White"));
+		c.add(new Characters("Professor Plum", "Purple"));
+		c.add(new Characters("Colonel Mustard", "Yellow"));
+		c.add(new Characters("Mrs.Peacock", "Blue"));
+		c.add(new Characters("Miss.Scarlet", "Red"));
+		c.add(new Characters("Mr.Green", "Green"));
+		return c;
 	}
 	
-	private static void Rooms(){
-		ArrayList<Rooms> rooms = new ArrayList<>();
-		rooms.add(new Rooms("Library", true));
-		rooms.add(new Rooms("Ballroom", true));
-		rooms.add(new Rooms("Kitchen", true));
-		rooms.add(new Rooms("Conservatory", true));
-		rooms.add(new Rooms("Billard Room", true));
-		rooms.add(new Rooms("Study", true));
-		rooms.add(new Rooms("Hall", true));
-		rooms.add(new Rooms("Lounge", true));
-		rooms.add(new Rooms("Dining Room", true));
+	private static ArrayList<Rooms> Rooms(ArrayList<Rooms> r){
+		r.add(new Rooms("Library", true));
+		r.add(new Rooms("Ballroom", true));
+		r.add(new Rooms("Kitchen", true));
+		r.add(new Rooms("Conservatory", true));
+		r.add(new Rooms("Billard Room", true));
+		r.add(new Rooms("Study", true));
+		r.add(new Rooms("Hall", true));
+		r.add(new Rooms("Lounge", true));
+		r.add(new Rooms("Dining Room", true));
+		return r;
 	}
 	
-	private void Weapons() {
-		ArrayList<Weapons> weapons = new ArrayList<>();
-		weapons.add(new Weapons("Wrench"));
-		weapons.add(new Weapons("Candlestick"));
-		weapons.add(new Weapons("Lead Pipe"));
-		weapons.add(new Weapons("Rope"));
-		weapons.add(new Weapons("Revolver"));
-		weapons.add(new Weapons("Dagger (Knife)"));
+	private ArrayList<Weapons> Weapons(ArrayList<Weapons> w) {
+		w.add(new Weapons("Wrench"));
+		w.add(new Weapons("Candlestick"));
+		w.add(new Weapons("Lead Pipe"));
+		w.add(new Weapons("Rope"));
+		w.add(new Weapons("Revolver"));
+		w.add(new Weapons("Dagger (Knife)"));
+		return w;
 	}
 	 
 	public Card()
@@ -49,7 +52,7 @@ public abstract class Card {
 	
 	public void Solution()
 	{
-		//what the fuck is going on?
+		 Object[] solution = new Object[3];
 	}
 			
 }
