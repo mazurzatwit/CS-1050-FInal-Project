2131e079df026f11fd5db8ec3f7cfe95e784b883
mazurz <mazurz@BY1926A75000213.wit.private>
2020-03-27 12:17:20 -0400

    Deck Updated

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,62 @@
+import java.util.ArrayList;
+import java.util.Random;
 
-public class Deck {
+public abstract class Deck {
+	private Random rand = new Random();
+
+	 private ArrayList<Characters> characters = new ArrayList<>();
+	 private ArrayList<Rooms> rooms = new ArrayList<>();
+	 private ArrayList<Weapons> weapons = new ArrayList<>();
+
+
+	public ArrayList<Characters> Character(ArrayList<Characters> c){
+		c.add(new Characters("Mrs.White", "White"));
+		c.add(new Characters("Professor Plum", "Purple"));
+		c.add(new Characters("Colonel Mustard", "Yellow"));
+		c.add(new Characters("Mrs.Peacock", "Blue"));
+		c.add(new Characters("Miss.Scarlet", "Red"));
+		c.add(new Characters("Mr.Green", "Green"));
+		return c;
+	}
+	
+	public static ArrayList<Rooms> Rooms(ArrayList<Rooms> r){
+		r.add(new Rooms("Library", true));
+		r.add(new Rooms("Ballroom", true));
+		r.add(new Rooms("Kitchen", true));
+		r.add(new Rooms("Conservatory", true));
+		r.add(new Rooms("Billard Room", true));
+		r.add(new Rooms("Study", true));
+		r.add(new Rooms("Hall", true));
+		r.add(new Rooms("Lounge", true));
+		r.add(new Rooms("Dining Room", true));
+		return r;
+	}
+	
+	private ArrayList<Weapons> Weapons(ArrayList<Weapons> w) {
+		w.add(new Weapons("Wrench"));
+		w.add(new Weapons("Candlestick"));
+		w.add(new Weapons("Lead Pipe"));
+		w.add(new Weapons("Rope"));
+		w.add(new Weapons("Revolver"));
+		w.add(new Weapons("Dagger (Knife)"));
+		return w;
+	}
+	 
+	
+	public void Solution()
+	{
+		 Object[] solution = new Object[3];
+        int num=rand.nextInt(characters.size()); 
+        solution[0]=characters.get(num);
+        characters.remove(num);
+       
+        num=rand.nextInt(rooms.size());
+        solution[1]=rooms.get(num);
+        rooms.remove(num);
+        
+        num=rand.nextInt(weapons.size());
+        solution[2]=weapons.get(num);
+        weapons.remove(num);
+	}
 
 }
