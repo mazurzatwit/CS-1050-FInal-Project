b5cd6fb89307c033f70dccbc2ff1a6e49f14016f
sarahsingh <sarahsingh@sarahs-mbp.home>
2020-04-03 13:01:52 -0400

    we have tiny hands!!!

--- src/GameManager.java
+++ src/GameManager.java
@@ -40,21 +40,122 @@ public class GameManager extends Deck{
 		}
 		else if(numPlayers == 4)
 		{
+			String[] hand1 = new String[handSize];
+			String[] hand2 = new String[handSize];
+			String[] hand3 = new String[handSize];
+			String[] hand4 = new String[handSize];
 			
-		}
-		/*
-		if(handSize == 0) {
-			for(int i = 0; i <= handSize; i++)
+			for(int i = 0; i < newDeck.size(); i++)
 			{
-				String curr = newDeck.get(i);
-				holds.add(curr);
-				newDeck.remove(curr);
+				hand1[i] = newDeck.get(i);
+				newDeck.remove(i);
 			}
-		} else if (handSize != 0) {
+			for(int i = 0; i < newDeck.size(); i++)
+			{
+				hand2[i] = newDeck.get(i);
+				newDeck.remove(i);
+			}
+			for(int i = 0; i < newDeck.size(); i++)
+			{
+				hand3[i] = newDeck.get(i);
+				newDeck.remove(i);
+			}
+			for(int i = 0; i < newDeck.size(); i++) 
+			{
+				hand4[i] = newDeck.get(i);
+				newDeck.remove(i);
+			}
+			Hand h1 = new Hand(hand1);
+			Hand h2 = new Hand(hand2);
+			Hand h3 = new Hand(hand3);
+			Hand h4 = new Hand(hand4);
+		}
+		else if(numPlayers == 5) 
+		{
+			String[] hand1 = new String[handSize];
+			String[] hand2 = new String[handSize];
+			String[] hand3 = new String[handSize];
+			String[] hand4 = new String[handSize];
+			String[] hand5 = new String[handSize];
 			
-			// mod shuffle deck
-		} 
-		*/ 
+			for(int i = 0; i < newDeck.size(); i++)
+			{
+				hand1[i] = newDeck.get(i);
+				newDeck.remove(i);
+			}
+			for(int i = 0; i < newDeck.size(); i++)
+			{
+				hand2[i] = newDeck.get(i);
+				newDeck.remove(i);
+			}
+			for(int i = 0; i < newDeck.size(); i++)
+			{
+				hand3[i] = newDeck.get(i);
+				newDeck.remove(i);
+			}
+			for(int i = 0; i < newDeck.size(); i++) 
+			{
+				hand4[i] = newDeck.get(i);
+				newDeck.remove(i);
+			}
+			for(int i = 0; i < newDeck.size(); i++) 
+			{
+				hand5[i] = newDeck.get(i);
+				newDeck.remove(i);
+			}			
+			Hand h1 = new Hand(hand1);
+			Hand h2 = new Hand(hand2);
+			Hand h3 = new Hand(hand3);
+			Hand h4 = new Hand(hand4);
+			Hand h5 = new Hand(hand5);
+		}
+		else if(numPlayers == 6) 
+		{
+			String[] hand1 = new String[handSize];
+			String[] hand2 = new String[handSize];
+			String[] hand3 = new String[handSize];
+			String[] hand4 = new String[handSize];
+			String[] hand5 = new String[handSize];
+			String[] hand6 = new String[handSize];
+			
+			for(int i = 0; i < newDeck.size(); i++)
+			{
+				hand1[i] = newDeck.get(i);
+				newDeck.remove(i);
+			}
+			for(int i = 0; i < newDeck.size(); i++)
+			{
+				hand2[i] = newDeck.get(i);
+				newDeck.remove(i);
+			}
+			for(int i = 0; i < newDeck.size(); i++)
+			{
+				hand3[i] = newDeck.get(i);
+				newDeck.remove(i);
+			}
+			for(int i = 0; i < newDeck.size(); i++) 
+			{
+				hand4[i] = newDeck.get(i);
+				newDeck.remove(i);
+			}
+			for(int i = 0; i < newDeck.size(); i++) 
+			{
+				hand5[i] = newDeck.get(i);
+				newDeck.remove(i);
+			}
+			for(int i = 0; i < newDeck.size(); i++) {
+				hand6[i] = newDeck.get(i);
+				newDeck.remove(i);
+			}
+			Hand h1 = new Hand(hand1);
+			Hand h2 = new Hand(hand2);
+			Hand h3 = new Hand(hand3);
+			Hand h4 = new Hand(hand4);
+			Hand h5 = new Hand(hand5);
+			Hand h6 = new Hand(hand6);
+		}
+		
+		
 	}
 	
 	
